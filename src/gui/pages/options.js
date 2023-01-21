d6fe54f907bc6154534dfca920e4be542ad399a5
Michael Gombos <71684424+MichaelGombos@users.noreply.github.com>
2023-01-21 15:31:51 -0600

    add safetey net for localStorage clear button

--- src/gui/pages/options.js
+++ src/gui/pages/options.js
@@ -8,9 +8,10 @@ const {useState} = React
 const Options = ({setter,previous}) => {
   let [newEnableGhost, setNewEnableGhost] = useState(getEnableGhost());
   let [newParticleLimit,setNewParticleLimit] = useState(getParticleLimit());
+  let [gameDataSafeteyNet,setGameDataSafeteyNet] = useState(3);
 
   return (
-    <div className="menu options">
+    <div className={`menu options ${previous == "main" ? "from-main" : null}`}>
     <h2>options</h2>
     <label htmlFor="ghost-selector">Enable Ghost Car  </label>
     <button  
@@ -19,7 +20,13 @@ const Options = ({setter,previous}) => {
 
     <label htmlFor="particle-selector">Particle Limit ({newParticleLimit})</label>
     <input type="range" min="0" max="2000" value={newParticleLimit}  className="slider" id="particle-selector" onChange={(e) => {setNewParticleLimit(e.target.value)}}/>
-    <button className="delete-save" onClick={localStorage.clear()}>Clear game data</button>
+    <button className={gameDataSafeteyNet > 0 ? "delete-save" : "disabled"} onClick={() => {
+      setGameDataSafeteyNet(gameDataSafeteyNet-1)
+      if(gameDataSafeteyNet == 1){
+        console.log("this is when...")
+        localStorage.clear();
+      }
+    }}>{gameDataSafeteyNet > 0 ? `Click ${gameDataSafeteyNet} times to delete game data` : "game Data deleted"}</button>
     <button onClick={() => {
       setter(previous);
       setNewEnableGhost(getEnableGhost());
