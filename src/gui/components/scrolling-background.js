92b8eeab126d3fdc283b73227744255f840fb8b3
Michael Gombos <71684424+MichaelGombos@users.noreply.github.com>
2023-02-24 16:08:21 -0600

    update background component to be parralax

--- src/gui/components/scrolling-background.js
+++ src/gui/components/scrolling-background.js
@@ -1,18 +1,34 @@
 import React from 'react';
-import background from "../../assets/background.png"
+
+import layer1 from "../../assets/background/1.png"
+import layer2 from "../../assets/background/2.png"
+import layer3 from "../../assets/background/3.png"
+import layer4 from "../../assets/background/4.png"
+import layer5 from "../../assets/background/5.png"
 
 const ScrollingBackground = () => {
 
   return (
     <div className='background'>
-      <div>
-        <img src={background}/>
-        <img src={background}/>
-        <img src={background}/>
-        <img src={background}/>
-        <img src={background}/>
-        <img src={background}/>
-        <img src={background}/>
+      <div className='background-layer-1'>
+        <img src={layer1}/>
+        <img src={layer1}/>
+      </div>
+      <div className='background-layer-2'>
+        <img src={layer2}/>
+        <img src={layer2}/>
+      </div>
+      <div className='background-layer-3'>
+        <img src={layer3}/>
+        <img src={layer3}/>
+      </div>
+      <div className='background-layer-4'>
+        <img src={layer4}/>
+        <img src={layer4}/>
+      </div>
+      <div className='background-layer-5'>
+        <img src={layer5}/>
+        <img src={layer5}/>
       </div>
     </div>
   )
