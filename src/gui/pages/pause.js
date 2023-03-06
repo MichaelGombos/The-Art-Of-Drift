d3195070e36b2965bfd6cccf34ad56d4587c5b59
Michael Gombos <71684424+MichaelGombos@users.noreply.github.com>
2023-03-05 20:29:28 -0600

    update pause screen design

--- src/gui/pages/pause.js
+++ src/gui/pages/pause.js
@@ -2,27 +2,39 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import { getGameMapIndex } from '../../game/game.js';
 import {turnOffGame, unPauseGame} from "../../game/main.js"
+import Button from '../components/button.js';
 
 const Pause = ({setPrevious}) => {
   const navigate = useNavigate();
 
   return (
-    <div className="menu paused">
-      <h1 className="f-h2">PAUSED</h1>
-      <div>
-        <button onClick={() => {
-          navigate("/hidden");
-        unPauseGame();
-        }}>Return to game</button>
-        <button onClick={() => {
-          setPrevious("pause")
-          navigate("/settings");
-        }}>Settings</button>
-        <button onClick={() => {
-          navigate("/main");
-          turnOffGame();
-        }}>Back to main menu</button>
+    <div className='menu-container'>
+      <div className="paused-menu col-2 align-center gap-lg">
+        <h1 className="f-h1">Pause</h1>
+        <div className='col-6 align-center gap-md' >
+          <Button style="primary" clickHandler={() => {
+            navigate("/hidden");
+          unPauseGame();
+          }}>Return to game</Button>
+          <Button clickHandler={() => {
+            setPrevious("pause")
+            navigate("/settings");
+          }}>Settings</Button>
+          {
+            getGameMapIndex() &&
+            <Button clickHandler={() => {
+              navigate(`/campaign/${getGameMapIndex()}`);
+              turnOffGame();
+            }}>Back to level screen</Button>
+          }
+
+          <Button clickHandler={() => {
+            navigate("/main");
+            turnOffGame();
+          }}>Back to main menu</Button>
+        </div>
       </div>
     </div>
   )
