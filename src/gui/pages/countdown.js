bf14122c6739158d983d3b0c7cdacd591380cb40
Michael Gombos <71684424+MichaelGombos@users.noreply.github.com>
2023-04-08 15:06:24 -0500

    fix game freezing after countdown

--- src/gui/pages/countdown.js
+++ src/gui/pages/countdown.js
@@ -19,7 +19,7 @@ const Countdown = () => {
 
     if(counter == 0){
       navigate("/hidden");
-      unPauseGame();
+      resetGame();
       console.log("just unpaused on god", getRunning())
     }
   }, [counter]);
